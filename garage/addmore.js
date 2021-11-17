5d7a2393170c49fb64d4d51966b4f135bc3a961f
zina <keiraayari@gmail.com>
2021-11-17 09:56:47 +0100

    mise a jour

---
+++ garage/addmore.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/garage_DB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).catch(err => console.log(err));
+
+const carSchema = new mongoose.Schema({
+
+    brand: String,
+    model: String,
+    year: Number,
+    create: { type: Date, default: Date.now },
+});
+console.log('car', carSchema);
+
+const Car = mongoose.model('Car', carSchema);
+
+Car.insertMany([ 
+    { brand: 'Aston Martin', model: 'DB9', year: 2010}, 
+    { brand: 'Range Rover', model: 'Discovery sport', year: 2017},
+]).then(function(){ 
+    console.log("Data inserted") 
+}).catch(function(error){ 
+    console.log(error);
+}); 
