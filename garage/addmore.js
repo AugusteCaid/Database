2c9560b9e4d042d92c73af62caeb8ea03257f68b
zina <keiraayari@gmail.com>
2021-11-17 11:04:35 +0100

    rajout Megane

--- garage/addmore.js
+++ garage/addmore.js
@@ -15,11 +15,13 @@ console.log('car', carSchema);
 
 const Car = mongoose.model('Car', carSchema);
 
-Car.insertMany([ 
-    { brand: 'Aston Martin', model: 'DB9', year: 2010}, 
-    { brand: 'Range Rover', model: 'Discovery sport', year: 2017},
-]).then(function(){ 
-    console.log("Data inserted") 
-}).catch(function(error){ 
+Car.insertMany([
+    { brand: 'Aston Martin', model: 'DB9', year: 2010 },
+    { brand: 'Range Rover', model: 'Discovery sport', year: 2017 },
+    { brand: 'Renault', model: 'Megane', year: 2016 },
+
+]).then(function () {
+    console.log("Data inserted")
+}).catch(function (error) {
     console.log(error);
-}); 
+});
